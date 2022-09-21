55acf781a143d749c2fa9f807dc1714f0a49d7ae
raniere57 <raaniere.r@gmail.com>
2022-09-21 15:50:35 -0300

    feat: configurado tema claro e provider

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,11 @@
+import { Button } from '@mui/material'
 import {Navigate, Route, Routes} from 'react-router-dom'
 
 export const AppRoutes = () => {
 
   return(
     <Routes>
-      <Route path='pagina-inicial' element={<p>ppágina inicial</p>} />
+      <Route path='pagina-inicial' element={<Button variant='contained' color='primary' >botaoooo</Button>} />
       <Route path='*' element={<Navigate to='/pagina-inicial' />} />
     </Routes>
   )
