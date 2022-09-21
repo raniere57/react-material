aacf14f4c22a41a2b9bc2e38d7874fd51e854ca3
raniere57 <raaniere.r@gmail.com>
2022-09-21 19:04:51 -0300

    feat: configurado eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import {BrowserRouter} from 'react-router-dom'
-import { AppRoutes } from './routes'
-import { AppThemeProvider } from './shared/contexts'
+import { BrowserRouter } from 'react-router-dom';
+import { AppRoutes } from './routes';
+import { AppThemeProvider } from './shared/contexts';
 
 export const App = () => {
 
   return (
 
-      <AppThemeProvider>
-        <BrowserRouter>
-          <AppRoutes/>
-        </BrowserRouter>
-      </AppThemeProvider>
-  )
-}
+    <AppThemeProvider>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </AppThemeProvider>
+  );
+};
--- src/shared/contexts/ThemeContext.tsx
+++ src/shared/contexts/ThemeContext.tsx
@@ -1,6 +1,6 @@
-import React, { createContext, useCallback, useContext, useMemo, useState } from "react";
-import { Box, ThemeProvider } from '@mui/material'
-import { DarkTheme, LightTheme } from '../../shared/themes'
+import React, { createContext, useCallback, useContext, useMemo, useState } from 'react';
+import { Box, ThemeProvider } from '@mui/material';
+import { DarkTheme, LightTheme } from '../../shared/themes';
 
 
 
@@ -13,34 +13,34 @@ interface IThemeProviderProps {
   children: React.ReactNode
 }
 
-const ThemeContext = createContext({} as IThemeContextData)
+const ThemeContext = createContext({} as IThemeContextData);
 
 export const useAppThemeContext = () => {
-  return useContext(ThemeContext)
-}
+  return useContext(ThemeContext);
+};
 
-export const AppThemeProvider: React.FC<IThemeProviderProps> = ({children}) => {
+export const AppThemeProvider: React.FC<IThemeProviderProps> = ({ children }) => {
 
-  const [themeName, setThemeName] = useState<'light' | 'dark'>('light')
+  const [themeName, setThemeName] = useState<'light' | 'dark'>('light');
 
   const toggleTheme = useCallback(() => {
-    setThemeName(oldThemeName => oldThemeName === 'light' ? 'dark' : 'light')
-  }, [])
+    setThemeName(oldThemeName => oldThemeName === 'light' ? 'dark' : 'light');
+  }, []);
 
   const theme = useMemo(() => {
-    if (themeName === 'light') return LightTheme
+    if (themeName === 'light') return LightTheme;
 
-    return DarkTheme
-  }, [themeName])
+    return DarkTheme;
+  }, [themeName]);
 
-  return(
-    <ThemeContext.Provider value={{themeName, toggleTheme}}>
+  return (
+    <ThemeContext.Provider value={{ themeName, toggleTheme }}>
       <Box width='100vw' height='100vh' bgcolor={theme.palette.background.default} >
         <ThemeProvider theme={theme} >
           {children}
         </ThemeProvider>
       </Box>
     </ThemeContext.Provider>
-  )
-}
+  );
+};
 
