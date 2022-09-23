341ca6cbf72b413e2a7cd9ae0a92f276527bf952
raniere57 <raaniere.r@gmail.com>
2022-09-23 17:05:47 -0300

    feat: Consulta backend e listagem no React

--- src/pages/pessoas/ListagemDePessoas.tsx
+++ src/pages/pessoas/ListagemDePessoas.tsx
@@ -1,10 +1,11 @@
-import { useMemo } from 'react';
+import { useEffect, useMemo } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import { FerramentasDaListagem } from '../../shared/components';
 import { LayoutBaseDePaginas } from '../../shared/layouts';
+import { PessoasService } from '../../shared/services/api/pessoas/PessoasService';
 
 
-export const ListagemDeCidade = () => {
+export const ListagemDePessoas = () => {
 
   const [searchParams, setSearchParams] = useSearchParams();
 
@@ -12,9 +13,21 @@ export const ListagemDeCidade = () => {
     return searchParams.get('busca') || '';
   }, [searchParams]);
 
+  useEffect(() => {
+    PessoasService.getAll(1, busca)
+      .then((result) => {
+        if(result instanceof Error) {
+          alert(result.message);
+          return;
+        } else {
+          console.log(result);
+        }
+      });
+  }, [busca]);
+
   return (
     <LayoutBaseDePaginas
-      titulo='Listagem de cidades'
+      titulo='Listagem de pessoas'
       barraDeFerramentas={
         <FerramentasDaListagem
           textoBotaoNovo='Nova'
