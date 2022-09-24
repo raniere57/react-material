ab9cab090940dd743dd1be9674f3956f84a1d966
raniere57 <raaniere.r@gmail.com>
2022-09-24 09:44:44 -0300

    feat: inserido paginação na listagem de pessoas

--- src/pages/pessoas/ListagemDePessoas.tsx
+++ src/pages/pessoas/ListagemDePessoas.tsx
@@ -1,29 +1,50 @@
-import { useEffect, useMemo } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
-import { FerramentasDaListagem } from '../../shared/components';
-import { LayoutBaseDePaginas } from '../../shared/layouts';
-import { PessoasService } from '../../shared/services/api/pessoas/PessoasService';
 
+import { Icon, IconButton, LinearProgress, Pagination, Paper, Table, TableBody, TableCell, TableContainer, TableFooter, TableHead, TableRow } from '@mui/material';
+
+import { FerramentasDaListagem } from '../../shared/components';
+import { useDebounce } from '../../shared/hooks';
+import { LayoutBaseDePaginas } from '../../shared/layouts';
+import { IListagemPessoa, PessoasService } from '../../shared/services/api/pessoas/PessoasService';
+import { Environment } from '../../shared/environment';
 
 export const ListagemDePessoas = () => {
 
   const [searchParams, setSearchParams] = useSearchParams();
 
+  const { debounce } = useDebounce();
+
+  const [rows, setRows] = useState<IListagemPessoa[]>([]);
+  const [totalCount, setTotalCount] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
+
   const busca = useMemo(() => {
     return searchParams.get('busca') || '';
   }, [searchParams]);
 
+  const pagina = useMemo(() => {
+    return Number(searchParams.get('pagina') || '1');
+  }, [searchParams]);
+
   useEffect(() => {
-    PessoasService.getAll(1, busca)
-      .then((result) => {
-        if(result instanceof Error) {
-          alert(result.message);
-          return;
-        } else {
-          console.log(result);
-        }
-      });
-  }, [busca]);
+    setIsLoading(true);
+    debounce(() => {
+      PessoasService.getAll(pagina, busca)
+        .then((result) => {
+          setIsLoading(false);
+          if (result instanceof Error) {
+            alert(result.message);
+            return;
+          } else {
+            console.log(result);
+
+            setTotalCount(result.totalCount);
+            setRows(result.data);
+          }
+        });
+    });
+  }, [busca, pagina]);
 
   return (
     <LayoutBaseDePaginas
@@ -33,10 +54,60 @@ export const ListagemDePessoas = () => {
           textoBotaoNovo='Nova'
           mostrarInputBusca
           textoDaBusca={busca}
-          aoMudarTextoDaBusca={texto => setSearchParams({ busca: texto }, { replace: true })}
+          aoMudarTextoDaBusca={texto => setSearchParams({ busca: texto, pagina: '1' }, { replace: true })}
         />}
     >
+      <TableContainer component={Paper} variant='outlined' sx={{ m: 1, width: 'auto' }}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Ações</TableCell>
+              <TableCell>Nome Completo</TableCell>
+              <TableCell>Email</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map(row => (
+              <TableRow key={row.id}>
+                <TableCell>
+                  <IconButton>
+                    <Icon>
 
+                    </Icon>
+                  </IconButton>
+                </TableCell>
+                <TableCell>{row.nomeCompleto}</TableCell>
+                <TableCell>{row.email}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+
+          {totalCount === 0 && !isLoading && (
+            <caption>{Environment.LISTAGEM_VAZIA}</caption>
+          )}
+
+          <TableFooter>
+            {isLoading && (
+              <TableRow>
+                <TableCell colSpan={3}>
+                  <LinearProgress variant='indeterminate' />
+                </TableCell>
+              </TableRow>
+            )}
+            {(totalCount > 0 && totalCount > Environment.LIMITE_DE_LINHAS) && (
+              <TableRow>
+                <TableCell colSpan={3}>
+                  <Pagination
+                    count={Math.ceil(totalCount / Environment.LIMITE_DE_LINHAS)}
+                    page={pagina}
+                    onChange={(e, newPage) => setSearchParams({ busca, pagina: newPage.toString() }, { replace: true })}
+                  />
+                </TableCell>
+              </TableRow>
+            )}
+          </TableFooter>
+        </Table>
+      </TableContainer>
     </LayoutBaseDePaginas>
   );
 };
--- src/shared/services/api/pessoas/PessoasService.ts
+++ src/shared/services/api/pessoas/PessoasService.ts
@@ -1,14 +1,14 @@
 import { Environment } from '../../../environment';
 import { Api } from '../axios-config';
 
-interface IListagemPessoa {
+export interface IListagemPessoa {
   id: number
   nomeCompleto: string
   email: string
   cidadeId: number
 }
 
-interface IDetalhePessoa {
+export interface IDetalhePessoa {
   id: number
   nomeCompleto: string
   email: string
@@ -22,7 +22,7 @@ type TPessoasComTotalCount = {
 
 const getAll = async (page = 1, filter = ''): Promise<TPessoasComTotalCount | Error> => {
   try {
-    const urlRelativa = `/pessoas?_page${page}&_limit=${Environment.LIMITE_DE_LINHAS}&nomeCompleto_like${filter}`;
+    const urlRelativa = `/pessoas?_page=${page}&_limit=${Environment.LIMITE_DE_LINHAS}&nomeCompleto_like=${filter}`;
     const { data, headers } = await Api.get(urlRelativa);
     if (data) {
       return {
